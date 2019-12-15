21e310e0a7b5c64f0d6f94787473f0a728ef5447
xiaweiqiang <xiaweiqiang@yunzhangfang.com>
2019-12-15 20:50:40 +0800

    init gshop

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '../pages/Home/Home'
+import Search from '../pages/Search/Search'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+import Login from '../pages/Login/Login'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/home',
+      component: Home,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
+})
